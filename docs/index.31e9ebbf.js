940befe63be72e5de18ee33aa800dcd1154f53ed
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2023-11-30 23:10:19 +0100

    commit

---
+++ docs/index.31e9ebbf.js
@@ -0,0 +1,14 @@
+(()=>{
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open-s]"),
+        closeModalBtn: document.querySelector("[data-modal-close-s]"),
+        modal: document.querySelector("[data-modal-s]")
+    };
+    refs.openModalBtn.addEventListener("click", toggleModalSCB);
+    refs.closeModalBtn.addEventListener("click", toggleModalSCB);
+    function toggleModalSCB() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+//# sourceMappingURL=index.31e9ebbf.js.map
