54efbcf5467372067f4d5f503a41b124ac3292ed
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2023-12-01 23:59:56 +0100

    commit

---
+++ docs/index.66caa261.js
@@ -0,0 +1,2 @@
+new Swiper(".slide-content",{slidesPerView:1,spaceBetween:25,loop:!0,centerSlide:"true",fade:"true",grabCursor:"true",pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!1},breakpoints:{0:{slidesPerView:1},769:{slidesPerView:2.5},1201:{slidesPerView:4}}});
+//# sourceMappingURL=index.66caa261.js.map
