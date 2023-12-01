4a869733016f44636d14e9e7d2c4f79c3632f253
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2023-12-01 20:47:50 +0100

    commit

---
+++ docs/index.a15b6cec.js
@@ -0,0 +1,2 @@
+new Swiper2(".mySwiper2",{slidesPerView:3,spaceBetween:28,pagination:{el:".swiper2-pagination",clickable:!0,clickable:!0,dynamicBullets:!1},breakpoints:{0:{slidesPerView:1},769:{slidesPerView:2},1201:{slidesPerView:3}}});
+//# sourceMappingURL=index.a15b6cec.js.map
