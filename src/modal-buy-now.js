e020b2c59d41a23af453cabf1fe0be663575c8e6
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-13 12:12:40 +0100

    commit

--- src/modal-buy-now.js
+++ src/modal-buy-now.js
@@ -2,14 +2,13 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-b]'),
     closeModalBtn: document.querySelector('[data-modal-close-b]'),
-    additionalBtn: document.querySelector('[data-modal-additional-b]'), // Dodany przycisk
+    additionalBtn: document.querySelector('[data-modal-additional-b]'), 
     modal: document.querySelector('[data-modal-b]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModalBuy);
   refs.closeModalBtn.addEventListener('click', toggleModalBuy);
-  refs.additionalBtn.addEventListener('click', toggleModalBuy); // Dodany nasłuchiwacz dla nowego przycisku
-
+  refs.additionalBtn.addEventListener('click', toggleModalBuy); 
   function toggleModalBuy() {
     refs.modal.classList.toggle('is-hidden');
   }
