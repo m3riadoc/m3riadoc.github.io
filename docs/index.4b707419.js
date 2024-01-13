d401171304f8ae5cb2fbaca845e916199bf8a6e4
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-13 04:05:19 +0100

    commit

--- docs/index.4b707419.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e={openModalBtn:document.querySelector("[data-modal-open-s]"),closeModalBtn:document.querySelector("[data-modal-close-s]"),modal:document.querySelector("[data-modal-s]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.4b707419.js.map
